ec820bbd610052892751b0fb8589b247ddd11abf
LilianBittar <bittarlily@gmail.com>
2022-11-09 22:09:18 +0100

    created custom user claims.

---
+++ HR.LeaveManagement.Application/Constants/CustomClaimTypes.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Constants
+{
+    public static class CustomClaimTypes
+    {
+        public const string Uid = "uid";
+    }
+}
